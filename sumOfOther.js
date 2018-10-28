31a3da45b752f90d3a8313f98968763fed67ff9c
GoldenKit <nikitoska01@gmail.com>
2018-10-28 14:59:18 +0300

    perf:improve performance of 'for'

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,9 +1,9 @@
 function sumOfOther(arr) {
 let newArr = [];
 let buf;
-for (let i=0;i<arr.length;i++){
-let red=arr.reduce(function(x,y) {return x+y;});
-buf=red-arr[i];
+let red = arr.reduce(function(x,y) { return x + y; });
+for (let i = 0, n = arr.length; i < n; i++){
+buf = red - arr[i];
 newArr.push(buf);
 }
 return newArr;
